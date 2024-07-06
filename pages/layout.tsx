2af251951c0e5a4e9b293ab85b813121f7547f7e
Georgiafab <2369596999@qq.com>
2024-07-06 10:43:16 +0800

    css

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Image from 'next/image';
 import SideBar from '../components/SideBar';
 import { Session } from 'next-auth';
-import MHeader from '../components/MHeader';
 type Props = {
     children: React.ReactElement,
     pathname: string,
